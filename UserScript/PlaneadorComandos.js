0da4989fc8da5eeaf7c0250aedf621b005a51483
Francisco Azevedo <franciscoazevedo@criticalmanufacturing.com>
2026-07-03 10:33:49 +0100

    Added Scripts

---
+++ UserScript/PlaneadorComandos.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name        PlaneadorComandos
+// @include     https://*tribalwars*screen=place*try=confirm*
+// @require     https://code.jquery.com/jquery-2.2.4.min.js
+// @downloadURL https://raw.githubusercontent.com/victorgare/tribalwars/master/UserScript/PlanejadorComandos.user.js
+// @updateURL   https://github.com/wasdfra/TWScripts/raw/master/UserScript/PlaneadorComandos.user.js
+// @run-at      document-start
+// ==/UserScript==
+
+CommandSender = {
+    confirmButton: null,
+    duration: null,
+    dateNow: null,
+    offset: null,
+    init: function() {
+        $($('#command-data-form')['find']('tbody')[0])['append']('<tr><td>Chegada:</td><td> <input type="datetime-local" id="CStime" step=".001"> </td></tr><tr> <td>Offset:</td><td> <input type="number" id="CSoffset"> <button type="button" id="CSbutton" class="btn">Confirmar</button> </td></tr>');
+        this['confirmButton'] = $('#troop_confirm_submit');
+        this['duration'] = $('#command-data-form')['find']('td:contains("Dura\xE7\xE3o:")')['next']()['text']()['split'](':')['map'](Number);
+        this['offset'] = localStorage['getItem']('CS.offset') || -250;
+        this['dateNow'] = this['convertToInput'](new Date());
+        $('#CSoffset')['val'](this['offset']);
+        $('#CStime')['val'](this['dateNow']);
+        $('#CSbutton')['click'](function() {
+            var _0xbbd0x1 = Number($('#CSoffset')['val']());
+            var _0xbbd0x2 = CommandSender['getAttackTime']();
+            localStorage['setItem']('CS.offset', _0xbbd0x1);
+            CommandSender['confirmButton']['addClass']('btn-disabled');
+            setTimeout(function() {
+                CommandSender['confirmButton']['click']()
+            }, _0xbbd0x2 - Timing['getCurrentServerTime']() + _0xbbd0x1);
+            this['disabled'] = true
+        })
+    },
+    getAttackTime: function() {
+        var _0xbbd0x3 = new Date($('#CStime')['val']()['replace']('T', ' '));
+        _0xbbd0x3['setHours'](_0xbbd0x3['getHours']() - this['duration'][0]);
+        _0xbbd0x3['setMinutes'](_0xbbd0x3['getMinutes']() - this['duration'][1]);
+        _0xbbd0x3['setSeconds'](_0xbbd0x3['getSeconds']() - this['duration'][2]);
+        return _0xbbd0x3
+    },
+    convertToInput: function(_0xbbd0x4) {
+        _0xbbd0x4['setHours'](_0xbbd0x4['getHours']() + this['duration'][0]);
+        _0xbbd0x4['setMinutes'](_0xbbd0x4['getMinutes']() + this['duration'][1]);
+        _0xbbd0x4['setSeconds'](_0xbbd0x4['getSeconds']() + this['duration'][2]);
+        var a = {
+            y: _0xbbd0x4['getFullYear'](),
+            m: _0xbbd0x4['getMonth']() + 1,
+            d: _0xbbd0x4['getDate'](),
+            time: _0xbbd0x4['toTimeString']()['split'](' ')[0],
+            ms: _0xbbd0x4['getMilliseconds']()
+        };
+        if (a['m'] < 10) {
+            a['m'] = '0' + a['m']
+        };
+        if (a['d'] < 10) {
+            a['d'] = '0' + a['d']
+        };
+        if (a['ms'] < 100) {
+            a['ms'] = '0' + a['ms'];
+            if (a['ms'] < 10) {
+                a['ms'] = '0' + a['ms']
+            }
+        };
+        return a['y'] + '-' + a['m'] + '-' + a['d'] + 'T' + a['time'] + '.' + a['ms']
+    },
+    addGlobalStyle: function(_0xbbd0x6) {
+        var _0xbbd0x7, _0xbbd0x8;
+        _0xbbd0x7 = document['getElementsByTagName']('head')[0];
+        if (!_0xbbd0x7) {
+            return
+        };
+        _0xbbd0x8 = document['createElement']('style');
+        _0xbbd0x8['type'] = 'text/css';
+        _0xbbd0x8['innerHTML'] = _0xbbd0x6;
+        _0xbbd0x7['appendChild'](_0xbbd0x8)
+    }
+};
+CommandSender['addGlobalStyle']('#CStime, #CSoffset {font-size: 9pt;font-family: Verdana,Arial;}#CSbutton {float:right;}');
+var a = setInterval(function() {
+    if (document['getElementById']('command-data-form') && jQuery) {
+        CommandSender['init']();
+        clearInterval(a)
+    }
+}, 1);
+setTimeout(function() {
+    $('#CSoffset')['eq'](0)['val']('-' + Timing['offset_to_server'])
+}, 2000)
---
+++ UserScript/Smart Farm.user.js
@@ -0,0 +1,176 @@
+// ==UserScript==
+// @name         Smart Farm
+// @version      2.0
+// @description  Smart farm script for better farming
+// @match        https://*.tribalwars.com.pt/*screen=am_farm*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tribalwars.com.br
+// @downloadURL  https://raw.githubusercontent.com/victorgare/tribalwars/master/UserScript/SmartFarm.user.js
+// @updateURL    https://github.com/victorgare/tribalwars/raw/master/UserScript/SmartFarm.user.js
+// @run-at document-end
+// @run-at document-idle
+// ==/UserScript==
+
+// Should skip villages with wall?
+// true = yes
+// false = no
+const SKIP_WALL = false;
+
+(function () {
+  'use strict';
+
+  const SmartFarm = new function () {
+    const TemplatesEnum = {
+      A: 'a',
+      B: 'b',
+      C: 'c'
+    }
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    const randomTime = (min, max) => {
+      return Math.round(min + Math.random() * (max - min));
+    };
+
+    const getTemplates = () => {
+      return Accountmanager.farm.templates;
+    };
+
+    const getCurrentUnits = () => {
+      return Accountmanager.farm.current_units;
+    };
+
+    const getNextVillage = () => {
+      // query only rows that are visible!
+      return document.querySelector(
+        "tr[id^='village_']:not([style='display: none;'])"
+      );
+    };
+
+    const hasLootedAll = (villageElement) => {
+      const lastLoot = villageElement.querySelector("img[src*='max_loot']");
+      return lastLoot && lastLoot.getAttribute("src").endsWith("1.webp");
+    };
+
+    const hasEnoughUnitsInTemplate = (template) => {
+      const units = getCurrentUnits();
+
+      for (const unitName in units) {
+        if (units.hasOwnProperty(unitName)) {
+          const unitQuantity = units[unitName];
+          const templateUnitQuantity = template[unitName];
+
+          if (templateUnitQuantity && unitQuantity < templateUnitQuantity) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    };
+
+    const getWallLevel = (villageElement) => {
+      return parseInt((villageElement.querySelectorAll("td")[6]).innerHTML);
+    }
+
+    const validateWall = (villageElement, toHide) => {
+      const wallLevel = getWallLevel(villageElement)
+      if (wallLevel !== NaN && wallLevel > 0) {
+         if(toHide)
+             villageElement.style.display = 'none';
+
+        return true
+      }
+
+      return false
+    }
+
+    const clickTemplate = (templateType, villageElement) => {
+      const selector = `a.farm_icon.farm_icon_${templateType}`;
+      const templateLink = villageElement.querySelector(selector);
+
+      if (templateLink) {
+        templateLink.click();
+      }
+    };
+
+    const validateAndSendTemplate = (template, villageElement, templateType) => {
+      if ( templateType === 'c' || hasEnoughUnitsInTemplate(template)) {
+        clickTemplate(templateType, villageElement);
+        return true;
+      }
+      return false;
+    };
+
+    const reloadPage = () => {
+      const reloadTime = randomTime(240000, 420000);
+      console.log(`will reload in ${reloadTime / 1000} seconds`);
+      setTimeout(() => {
+        console.log("reloading...");
+        window.location.reload();
+      }, reloadTime);
+    };
+
+    const sendAttack = async () => {
+
+      const templates = getTemplates();
+      if (!templates) return;
+
+      const [templateA, templateB] = Object.values(templates);
+      const villageElement = getNextVillage();
+
+
+      if (villageElement) {
+          if (SKIP_WALL) {
+              const result = validateWall(villageElement, true)
+              if (result)
+                  return;
+          }
+
+          var wallLevel = getWallLevel(villageElement);
+
+          if(wallLevel > 0){
+              if( wallLevel== 1){
+                  validateAndSendTemplate(templateB, villageElement, TemplatesEnum.B)
+              }
+              return;
+          }
+
+          if (hasLootedAll(villageElement)) {
+              if (!validateAndSendTemplate(templateA, villageElement, TemplatesEnum.C)) {
+                  validateAndSendTemplate(templateA, villageElement, TemplatesEnum.A);
+              }
+          } else {
+              validateAndSendTemplate(templateA, villageElement, TemplatesEnum.A);
+          }
+
+        const waitTime = randomTime(250, 350);
+        await delay(waitTime);
+      }
+    };
+
+
+    this.init = async () => {
+        await setInterval(5000);
+      console.log("starting farm");
+
+      // start the page reload
+      reloadPage();
+
+      setInterval(async () => {
+        await sendAttack();
+      }, 500)
+    };
+
+  };
+
+  $(function () {
+    setInterval( () => {
+        if (typeof Accountmanager !== 'undefined' && Accountmanager.farm) {
+            Accountmanager.farm.init();
+            SmartFarm.init();
+        } else {
+            console.error('Accountmanager or farm not defined');
+        }
+    },5000);
+  });
+})();
